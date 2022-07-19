16b33c7af14258036fbaaf62c5afd76541c5dbf2
Jonghee.Son <zzongaone@gmail.com>
2022-07-19 16:28:35 +0900

    Added comment for each function

--- src.c
+++ src.c
@@ -4,38 +4,39 @@
 #include <string.h>
 #include <MQTTClient.h>
 
-#define ADDRESS "tcp://localhost:1883" //Broker IP Address
-#define ADDRESS_SECURE "tcp://localhost:8883"
+#define ADDRESS "tcp://172.30.1.50:1883" //Broker IP Address
+#define ADDRESS_SECURE "tcp://172.30.1.50:8883" //Broker IP Address but with TLS encryption
 #define CLIENTID "CLIENT_SUB" //Client Name
-#define TOPIC_1 "lab/light" //Topic Configuration
-#define TOPIC_2 "light/stat"
-#define PAYLOAD_1 "Switch is ON"
+#define TOPIC_1 "lab/light" //Topic Configuration (Light)
+#define TOPIC_2 "light/stat" //Topic for current status of light
+#define PAYLOAD_1 "Switch is ON" //Defining message for status message publication
 #define PAYLOAD_2 "Switch is OFF"
 #define QOS 1 //QOS Configuration
-#define TIMEOUT 10000L
+#define TIMEOUT 10000L //Timeout required for finishing comm
 
-MQTTClient client;
+MQTTClient client; //Client handle
 MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
-MQTTClient_message pubmsg = MQTTClient_message_initializer;
+MQTTClient_message pubmsg = MQTTClient_message_initializer; //variable for MQTT message
 MQTTClient_deliveryToken token;
 
 const int outpin = 23; // HIGH-LOW OUTPUT Pin - Broadcom pin 23, P1 pin 16
-//char msgarray[5]; //Array for received message
 
 volatile MQTTClient_deliveryToken deliveredtoken;
 
 void delivered(void *context, MQTTClient_deliveryToken dt) {
+    //Gets delivery token from finished communication and spits out message below
     printf("Message with token value %d delivery confirmed\n", dt);
     deliveredtoken = dt;
 }
 
 void connlost(void *context, char *cause) {
+    //callback from connection lost event
     printf("\nConnection lost\n");
     printf("cause: %s\n", cause);
 }
 
 int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *message) {
-    //send received message to stdout
+    //send received message to preconfigured array msgarray[5] and parses command from that array
     int rc, ch;
     int i;
     char* payloadptr;
@@ -51,7 +52,7 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
 
     payloadptr = message->payload;
     for(i=0; i<message->payloadlen; i++) {
-        //putchar(*payloadptr++);
+        //getting received message from message pointer and saves each char to msgarray[5]
         msgarray[i] = *payloadptr++;
         putchar(msgarray[i]);
     }
@@ -59,6 +60,7 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
 
     MQTTClient_freeMessage(&message);
     MQTTClient_free(topicName);
+    //frees memory allocated to received message
 
     if (msgarray[0] == 'o' && msgarray[1] == 'n') {
         wiringPiSetupGpio(); //WringPi Initialization
@@ -69,12 +71,12 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
 
         printf("Switch is ON\n");
 
-        pubmsg.payload = PAYLOAD_1;
-        pubmsg.payloadlen = strlen(PAYLOAD_1);
+        pubmsg.payload = PAYLOAD_1; //loads predefined payload(PAYLOAD_1, lab/light) to pubmsg
+        pubmsg.payloadlen = strlen(PAYLOAD_1); 
         pubmsg.qos = QOS;
         pubmsg.retained = 0;
         deliveredtoken = 0;
-        MQTTClient_publishMessage(client, TOPIC_2, &pubmsg, &token);
+        MQTTClient_publishMessage(client, TOPIC_2, &pubmsg, &token); //publishes message to predefined topic(TOPIC_2, light/stat)
         rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
         printf("Status Message Delivered\n");
     } 
@@ -105,8 +107,10 @@ int main(int argc, char* argv[]) {
     MQTTClient_create(&client, ADDRESS, CLIENTID, MQTTCLIENT_PERSISTENCE_NONE, NULL); //create MQTTClient
     conn_opts.keepAliveInterval = 20;
     conn_opts.cleansession = 1;
+    //creates MQTTClient with client handle, address, client ID, etc.
 
-    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered); //setcallback for MQTT
+    MQTTClient_setCallbacks(client, NULL, connlost, msgarrvd, delivered); 
+    //sets callback functions for connection lost, message arrived, message delivered events
 
     if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS) {
         printf("Failed to connect, return code %d\n", rc);
@@ -114,11 +118,13 @@ int main(int argc, char* argv[]) {
     }
     printf("Subscribing to topic %s\nfor client %s using QoS%d\n", TOPIC_1, CLIENTID, QOS);
     MQTTClient_subscribe(client, TOPIC_1, QOS);
+    //subscribe for predefined topic (lab/light)
 
     do {
         ch = getchar();
     } while(ch!='Q' && ch != 'q');
     MQTTClient_disconnect(client, 10000);
     MQTTClient_destroy(&client);
+    //disconnect & destroy after receiving Q or q
     return rc;
 }
